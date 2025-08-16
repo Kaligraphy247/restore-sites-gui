348c71bf6842b5d8d3cb153ba1abe40446bf1cbc
kaligraphy247 <chikeziejames3@gmail.com>
2025-08-16 18:50:21 +0200

    Upgrade to Tauri v2 and Svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,103 +1,156 @@
 <script lang="ts">
-  // import { invoke } from "@tauri-apps/api/tauri";
+  import { invoke } from "@tauri-apps/api/core";
 
-  // let name = "";
-  // let greetMsg = "";
+  let name = $state("");
+  let greetMsg = $state("");
 
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   greetMsg = await invoke("greet", { name });
-  // }
-  type Site = {
-    alias: string;
-    incognito: boolean;
-    open: boolean;
-    url: string;
-  };
-
-  type SitesList = Array<Site>;
-
-  let sites: SitesList = [];
-  sites.push({
-    url: "https://github.com",
-    incognito: true,
-    open: true,
-    alias: "Github",
-  });
-  sites.push({
-    url: "https://google.com",
-    incognito: true,
-    open: true,
-    alias: "",
-  });
-  $: console.log(sites);
-
-  let siteUrl: string = "";
-  $: console.log(siteUrl);
-
-  function addSite() {
-    if (siteUrl === "") return;
-    sites = [
-      ...sites,
-      { url: siteUrl, incognito: true, open: true, alias: "" },
-    ];
-    // siteUrl = "";
-    console.log("sites", sites);
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
   }
 </script>
 
-<section class="py-4">
-  <div class="text-center">
-    <h1 class="text-4xl text-center font-semibold">Restore Sites GUI</h1>
-    <p class="text-lg my-1">
-      Restore previously open website to Incognito mode (Default)
-    </p>
-  </div>
+<main class="container">
+  <h1>Welcome to Tauri + Svelte</h1>
 
-  <div class="flex items-center justify-center space-x-2">
-    <input
-      type="text"
-      placeholder="URL"
-      class="text-black h-10 w-3/5 rounded pl-1"
-      bind:value={siteUrl}
-    />
-    <button
-      on:click={addSite}
-      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-      >Add</button
-    >
+  <div class="row">
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+    </a>
   </div>
+  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
-  <p class="text-lg my-1 mx-10 font-semibold">Total {sites.length}</p>
-  {#if sites.length === 0}
-    <div
-      class="flex items-center justify-center h-[400px] max-h-[400px] border mx-10 border-dashed rounded"
-    >
-      <h3 class="text-xl italic font-medium">No sites yet</h3>
-    </div>
-  {:else}
-    <ul
-      class="border mx-10 p-2 h-[400px] max-h-[400px] overflow-y-auto space-y-4"
-    >
-      {#each sites as site, index}
-        <li class="p-2 border rounded-lg text-lg">
-          {index + 1}.
-          {site.alias ? site.alias : site.url}
-          <!-- {site.url} -->
-          <input type="checkbox" bind:checked={site.incognito} disabled />
-          <input type="checkbox" bind:checked={site.open} disabled />
-          <button
-            class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-4 rounded"
-            >Delete</button
-          >
-        </li>
-      {/each}
-    </ul>
-  {/if}
-  <div class="mx-10 mt-4">
-    <button
-      class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
-      >Restore</button
-    >
-  </div>
-</section>
+  <form class="row" onsubmit={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+  <p>{greetMsg}</p>
+</main>
+
+<style>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.svelte-kit:hover {
+  filter: drop-shadow(0 0 2em #ff3e00);
+}
+
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  color: #0f0f0f;
+  background-color: #f6f6f6;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+.container {
+  margin: 0;
+  padding-top: 10vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: 0.75s;
+}
+
+.logo.tauri:hover {
+  filter: drop-shadow(0 0 2em #24c8db);
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+}
+
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+
+a:hover {
+  color: #535bf2;
+}
+
+h1 {
+  text-align: center;
+}
+
+input,
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  color: #0f0f0f;
+  background-color: #ffffff;
+  transition: border-color 0.25s;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+}
+
+button {
+  cursor: pointer;
+}
+
+button:hover {
+  border-color: #396cd8;
+}
+button:active {
+  border-color: #396cd8;
+  background-color: #e8e8e8;
+}
+
+input,
+button {
+  outline: none;
+}
+
+#greet-input {
+  margin-right: 5px;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    color: #f6f6f6;
+    background-color: #2f2f2f;
+  }
+
+  a:hover {
+    color: #24c8db;
+  }
+
+  input,
+  button {
+    color: #ffffff;
+    background-color: #0f0f0f98;
+  }
+  button:active {
+    background-color: #0f0f0f69;
+  }
+}
+
+</style>
